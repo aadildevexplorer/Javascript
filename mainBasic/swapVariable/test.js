827252145993b42e62e8f2377e60f5191676227d
aadildevexplorer <aadildevexplorer@gmail.com>
2026-06-27 16:18:34 +0530

    Solve logical qns

--- mainBasic/swapVariable/test.js
+++ mainBasic/swapVariable/test.js
@@ -1,14 +1,15 @@
 // ex - 1
+
+// let swap = a;
+// a = b;
+// b = swap;
+
+// console.log(a, b);
+
+// ex - 2
 let a = 10;
 b = 20;
 
-let swap = a;
-a = b;
-b = swap;
-
-console.log(a, b);
-
-// ex - 2
 [a, b] = [b, a];
 
 console.log(a, b);
